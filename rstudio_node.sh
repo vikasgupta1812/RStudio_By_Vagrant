a9afac22adeedfd1b8c787c4c47883687c1d5696
David Medinets <david.medinets@gmail.com>
2014-04-15 16:33:27 -0400

    Update configuration script to use latest version of R

--- rstudio_node.sh
+++ rstudio_node.sh
@@ -15,12 +15,16 @@ useradd -m -s /bin/bash -g rstudio rstudio -p $PASSWORD
 
 locale-gen en_US
 
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "deb http://lib.stat.cmu.edu/R/CRAN//bin/linux/ubuntu precise/" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+
 apt-get update
 apt-get install -y r-base gdebi-core libapparmor1
 
-wget -q http://download2.rstudio.org/rstudio-server-0.97.551-amd64.deb
+wget -q http://download2.rstudio.org/rstudio-server-0.98.501-amd64.deb
 
-gdebi -n rstudio-server-0.97.551-amd64.deb
+gdebi -n rstudio-server-0.98.501-amd64.deb
 
 # Do not let this run again
 touch /.vagrant_build_done
