788ba86778105b8a77e528ec86f1633613c54bd4
Vikas Gupta <vikas.ymca@gmail.com>
2014-04-24 17:42:17 -0500

    added installation for git

--- rstudio_node.sh
+++ rstudio_node.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+
+$(which git > /dev/null 2>&1)
+FOUND_GIT=$?
+if [ "$FOUND_GIT" -ne '0' ]; then
+  echo 'Attempting to install git.'
+  $(which apt-get > /dev/null 2>&1)
+  FOUND_APT=$?
+  $(which yum > /dev/null 2>&1)
+  FOUND_YUM=$?
+
+  if [ "${FOUND_YUM}" -eq '0' ]; then
+    yum -q -y makecache
+    yum -q -y install git
+    echo 'git installed.'
+  elif [ "${FOUND_APT}" -eq '0' ]; then
+    apt-get -q -y update
+    apt-get -q -y install git
+    echo 'git installed.'
+  else
+    echo 'No package installer available. You may need to install git manually.'
+  fi
+else
+  echo 'git found.'
+fi
+
+
 if [ -f /.vagrant_build_done ]; then
     echo "Script already run. Exiting."
     exit
